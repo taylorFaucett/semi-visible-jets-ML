d00b6468bd2584340c46c452cfeaa56f552ae301
taylorFaucett <tfaucett@uci.edu>
2020-09-10 13:01:03 -0700

    removed ipynb links

--- analysis/.ipynb_checkpoints/struc2arr-checkpoint.py
+++
@@ -1,6 +0,0 @@
-def struc2arr(x):
-    # pyjet outputs a structured array. This converts
-    # the 4 component structured array into a simple
-    # 4xN numpy array
-    x = x.view((float, len(x.dtype.names)))
-    return x

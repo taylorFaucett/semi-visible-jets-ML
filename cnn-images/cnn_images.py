1bb8cffd00f6502ffc8c0fc4fdcfa1495cf09b48
Taylor Faucett <tfaucett@uci.edu>
2020-12-04 05:00:10 -0700

    Sherpa code for CNN jet images

--- cnn-images/cnn_images.py
+++ cnn-images/cnn_images.py
@@ -1,21 +1,25 @@
-from tensorflow.keras import datasets, layers, models
-from sklearn.model_selection import train_test_split
+# Standard Imports
 import pandas as pd
 import h5py
 import numpy as np
-from sklearn import metrics
 import os
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, roc_curve
 
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-import tensorflow as tf
+# Keras Imports
+import keras
+from keras import metrics
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D
+from keras.datasets import mnist
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 
+# Path Imports
 import pathlib
-path = pathlib.Path.cwd()
 
-gpus = tf.config.experimental.list_physical_devices("GPU")
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+path = pathlib.Path.cwd()
 
 
 def scale_data(x, mean=True):
@@ -23,130 +27,178 @@ def scale_data(x, mean=True):
     std = np.std(x)
     if std == 0:
         std = 1
-    return (x-mean)/std
+    return (x - mean) / std
 
-def get_data(rinv, N):
-    df = h5py.File(path.parent / "data" / "jet_images" / f"LL-{rinv}.h5", "r")
-    y = df["targets"][:N]
-    x = df["features"][:N]
-    x = scale_data(x, mean=True)
-    x = np.expand_dims(x, axis=3)
-    return x, y
+
+def test_val_set(rinv, split):
+    f = h5py.File(path.parent / "data" / "jet_images" / f"LL-{rinv}.h5", "r")
+    dSize = f["targets"].shape[0]
+    train_size, val_size, test_size = (
+        int(np.floor(dSize * 0.80)),
+        int(np.floor(dSize * 0.1)),
+        int(np.floor(dSize * 0.1)),
+    )
+    if split == "val":
+        start = train_size
+        end = train_size + val_size
+    if split == "test":
+        start = train_size + val_size
+        end = -1
+
+    X = f["features"][start:end]
+    X = scale_data(X, mean=True)
+    X = np.expand_dims(X, axis=3)
+    y = f["targets"][start:end]
+    return dSize, X, y
+
+
+def generator(rinv, batch_size, fullSet=False):
+    while True:
+        f = h5py.File(path.parent / "data" / "jet_images" / f"LL-{rinv}.h5", "r")
+        N = f["targets"].shape[0]
+        start = 0
+        if not fullSet:
+            N = int(np.floor(N * 0.80))
+            
+        end = batch_size
+
+        while start < N:
+            X = f["features"][start:end]
+            X = scale_data(X, mean=True)
+            X = np.expand_dims(X, axis=3)
+            y = f["targets"][start:end]
+            yield X, y
+            start += batch_size
+            end += batch_size
+
 
 def plot_roc(X_test, y_test, rinv):
     auc_save_file = path / "roc_df" / f"auc-{rinv}.txt"
     test_predictions = model.predict(X_test).ravel()
-    auc = metrics.roc_auc_score(y_test, test_predictions)    
-    with open(auc_save_file, 'w') as f:
+    auc = roc_auc_score(y_test, test_predictions)
+    with open(auc_save_file, "w") as f:
         f.write(str(auc))
 
-    fpr, tpr, thresholds = metrics.roc_curve(y_test, test_predictions)
+    fpr, tpr, thresholds = roc_curve(y_test, test_predictions)
     background_efficiency = fpr
     signal_efficiency = tpr
-    background_rejection = 1. - background_efficiency
-    
-    roc_df = pd.DataFrame({"sig_eff": signal_efficiency, "bkg_eff": background_efficiency, "bkg_rej": background_rejection})
-    roc_df.to_csv(f'roc_df/{rinv}.csv')
-    #background_rejection = 1./fpr
+    background_rejection = 1.0 - background_efficiency
+
+    roc_df = pd.DataFrame(
+        {
+            "sig_eff": signal_efficiency,
+            "bkg_eff": background_efficiency,
+            "bkg_rej": background_rejection,
+        }
+    )
+    roc_df.to_csv(f"roc_df/{rinv}.csv")
+    # background_rejection = 1./fpr
     rinv_str = rinv.replace("p", ".")
-    plt.plot(signal_efficiency, background_rejection,
-         lw=2, label='$r_{inv} = %s$ ($AUC = %0.3f$)' %(rinv_str, auc))
-    plt.xlabel('Signal efficiency $(\epsilon_S)$')
-    plt.ylabel('Background rejection $(1 - \epsilon_B)$')
-    plt.title('ROC - CNN on Jet Images')
+    plt.plot(
+        signal_efficiency,
+        background_rejection,
+        lw=2,
+        label="$r_{inv} = %s$ ($AUC = %0.3f$)" % (rinv_str, auc),
+    )
+    plt.xlabel("Signal efficiency $(\epsilon_S)$")
+    plt.ylabel("Background rejection $(1 - \epsilon_B)$")
+    plt.title("ROC - CNN on Jet Images")
     plt.legend(loc="lower left")
     plt.savefig(path / "figures" / "cnn_roc.png")
     plt.savefig(path / "figures" / "cnn_roc.pdf")
     return auc
 
-def train_cnn(X, y, rinv, retrain=False):
+
+def train_cnn(rinv, retrain=False):
     # To retrain, remove the old model
     model_file = path / "models" / f"{rinv}.h5"
     if retrain and model_file.exists():
         os.remove(model_file)
-        
-    
-    # Split data for train, test, validation
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
-    
-    X_val, X_test, y_val, y_test = train_test_split(
-        X_val, y_val, test_size=0.5, random_state=42
-    )
 
     # If the model already exists (i.e. we haven't removed the last one) train a new model
-    if model_file.exists():
-        return tf.keras.models.load_model(model_file), X_test, y_test
-    else:
-        optimizer = tf.keras.optimizers.Adam(
-            learning_rate=0.001,
-            beta_1=0.9,
-            beta_2=0.999,
-            epsilon=1e-07,
-            amsgrad=False,
-            name="Adam",
+
+    optimizer = Adam(lr=0.001)
+
+    model = Sequential()
+    for lix in range(conv_layers):
+        model.add(
+            Conv2D(
+                conv_units,
+                (kernel_1, kernel_2),
+                activation=activation,
+                padding="same",
+                input_shape=(32, 32, 1),
+            )
         )
+        model.add(MaxPooling2D((pool_1, pool_2), padding="same"))
+    model.add(Flatten())
+    for lix in range(dense_layers):
+        model.add(Dense(dense_units, activation="relu"))
+        if lix <= dLayers - 2:
+            model.add(Dropout(dropout))
+    model.add(Dense(1, activation="sigmoid"))
 
-        model = models.Sequential()
-        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 1)))
-        model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(56, (3, 3), activation='relu'))
-        model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(56, (3, 3), activation='relu'))
-        #model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(56, (3, 3), activation='relu'))
-        model.add(layers.Flatten())
-        model.add(layers.Dense(178, activation='relu'))
-        model.add(tf.keras.layers.Dropout(0.5))
-        model.add(layers.Dense(178, activation='relu'))
-        model.add(tf.keras.layers.Dropout(0.5))
-        model.add(layers.Dense(178, activation='relu'))
-        model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
+    model.compile(
+        loss="binary_crossentropy",
+        optimizer=optimizer,
+        metrics=["accuracy", metrics.AUC(name="auc")],
+    )
+    callbacks = [
+        EarlyStopping(patience=10, verbose=1),
+        ModelCheckpoint(filepath=model_file, verbose=1, save_best_only=True),
+    ]
 
-        model.compile(optimizer=optimizer,
-                      loss="binary_crossentropy",
-                      metrics=[tf.keras.metrics.AUC(name="auc"),tf.keras.metrics.Accuracy(name="acc")]
-                     )
-        mc = tf.keras.callbacks.ModelCheckpoint(
-            model_file,
-            monitor="val_auc",
-            verbose=1,
-            save_best_only=True,
-            mode="max",
-        )
-        es = tf.keras.callbacks.EarlyStopping(
-            monitor="val_auc", mode="max", verbose=2, patience=10
-        )
-
-        history = model.fit(X_train, 
-                            y_train, 
-                            batch_size = 128, 
-                            epochs=250,
-                            verbose=2,
-                            validation_data=(X_val, y_val),
-                            callbacks=[mc, es],
-                           )
-        return model, X_test, y_test
-
+    training_data = generator(rinv=rinv, batch_size=batch_size)
+    N_size, X_test, y_test = test_val_set(rinv=rinv, split="test")
+    N_size, X_val, y_val = test_val_set(rinv=rinv, split="val")
     
-    
+    history = model.fit(
+        training_data,
+        epochs=200,
+        verbose=1,
+        steps_per_epoch = (N_size * 0.80) // batch_size,
+        validation_data=(X_val, y_val),
+        callbacks=callbacks,
+    )
+
+    return model, X_test, y_test
+
+
 if __name__ == "__main__":
     rinvs = ["0p0", "0p3", "1p0"]
-    N = 500000
+
+    batch_size = 500
     for rinv in rinvs:
-        # Grab jet images and labels
-        X, y = get_data(rinv, N)
+        # Import sherpa optimized hyperparamters for the rinv choice
+        sherpa_results = np.load(
+            f"sherpa_results/{rinv}.npy", allow_pickle="TRUE"
+        ).item()
+
+        # Set network parameters
+        learning_rate = sherpa_results["learning_rate"]
+        conv_units = sherpa_results["conv_units"]
+        dense_units = sherpa_results["dense_units"]
+        activation = sherpa_results["activation"]
+        dropout = sherpa_results["dropout"]
+        conv_layers = sherpa_results["conv_layers"]
+        dense_layers = sherpa_results["dense_layers"]
+        pool_1 = sherpa_results["pool_1"]
+        pool_2 = sherpa_results["pool_2"]
+        kernel_1 = sherpa_results["kernel_1"]
+        kernel_2 = sherpa_results["kernel_2"]
+        optimizer = Adam(lr=learning_rate)
 
         # Train a new model (or load the existing one if available)
-        model, X_test, y_test = train_cnn(X, y, rinv, retrain=False)
+        model, X_test, y_test = train_cnn(rinv, retrain=False)
 
         # Plot the ROC curve
         auc_val = plot_roc(X_test, y_test, rinv)
         print(rinv, auc_val)
 
         # Generate predictions for the full dataset
-        full_predictions = np.concatenate(model.predict(X))
-
-        # Save the predictions
-        np.save(path / "predictions" / "ll_predictions.npy", full_predictions)
+#         X = generator(rinv=rinv, batch_size=batch_size, fullSet=True)
+#         full_predictions = model.predict(X) #np.concatenate(model.predict(X))
+#         print(full_predictions)
+        
+#         # Save the predictions
+#         np.save(path / "predictions" / "ll_predictions.npy", full_predictions)
--- dnn-hl/dnn_sherpa.py
+++ dnn-hl/dnn_sherpa.py
@@ -1,4 +1,3 @@
-from tensorflow.keras import datasets, layers, models
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import h5py
@@ -7,45 +6,39 @@ import matplotlib.pyplot as plt
 from sklearn import metrics, preprocessing
 import os
 import matplotlib.pyplot as plt
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout
+from keras.datasets import mnist
+from keras.optimizers import Adam
+
 import sherpa
 import sherpa.algorithms.bayesian_optimization as bayesian_optimization
-
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-import tensorflow as tf
-
 import pathlib
+
 path = pathlib.Path.cwd()
 
-parameters = [sherpa.Continuous('learning_rate', [1e-4, 1e-2]),
-              sherpa.Discrete('num_units', [32, 128]),
-              sherpa.Choice('activation', ['relu', 'tanh', 'sigmoid'])]
+parameters = [
+    sherpa.Continuous("learning_rate", [1e-4, 1e-2]),
+    sherpa.Continuous("dropout", [0, 0.5]),
+    sherpa.Discrete("num_units", [32, 128]),
+    sherpa.Discrete("layers", [2, 8]),
+    sherpa.Choice("activation", ["relu", "tanh", "sigmoid"]),
+]
 algorithm = bayesian_optimization.GPyOpt(max_num_trials=50)
-study = sherpa.Study(parameters=parameters,
-                     algorithm=algorithm,
-                     lower_is_better=False)
 
-gpus = tf.config.experimental.list_physical_devices("GPU")
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
-
-def scale_data(x, mean=True):
-    mean = np.mean(x)
-    std = np.std(x)
-    if std == 0:
-        std = 1
-    return (x-mean)/std
-
-def get_data(rinv, N=None):        
+def get_data(rinv, N=None):
     hl_file = path.parent / "data" / "jss_observables" / f"HL-{rinv}.h5"
     x = pd.read_hdf(hl_file, "features")
     y = pd.read_hdf(hl_file, "targets")
 
     if N is not None:
-        x = x.loc[:N-1]
-        y = y.loc[:N-1]
-    for observable in list(x.columns):
-        if "c2" in observable or "c3" in observable or "d2" in observable:
-            x[observable] = np.log10(1.0+x[observable])
+        x = x.loc[: N - 1]
+        y = y.loc[: N - 1]
+    # for observable in list(x.columns):
+    #     if "c2" in observable or "c3" in observable or "d2" in observable:
+    #         x[observable] = np.log10(1.0 + x[observable])
 
     scaler = preprocessing.StandardScaler()
     x = scaler.fit_transform(x)
@@ -54,61 +47,68 @@ def get_data(rinv, N=None):
 
 def run_sherpa(X, y, rinv):
     # To retrain, remove the old model
-    model_file = path / "models" / f"{rinv}.h5"
-    epochs = 15
+    sherpa_results_file = path / 'sherpa_results' / f'{rinv}.npy'
+    if sherpa_results_file.exists():
+        return True
     
+    study = sherpa.Study(parameters=parameters, algorithm=algorithm, lower_is_better=False)
+    epochs = 25
+
     # Split data for train, test, validation
     X_train, X_val, y_train, y_val = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
-    
+
     X_val, X_test, y_val, y_test = train_test_split(
         X_val, y_val, test_size=0.5, random_state=42
     )
 
-
-
     for trial in study:
-        lr = trial.parameters['learning_rate']
-        num_units = trial.parameters['num_units']
-        act = trial.parameters['activation']
+        # Sherpa settings in trials
+        lr = trial.parameters["learning_rate"]
+        num_units = trial.parameters["num_units"]
+        activation = trial.parameters["activation"]
+        dropout = trial.parameters["dropout"]
+        layers = trial.parameters["layers"]
+        optimizer = Adam(lr=lr)
 
-        # Create model
-        model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(X_train.shape[1], )),
-                            tf.keras.layers.Dense(num_units, activation=act),
-                            tf.keras.layers.Dense(1, activation='sigmoid')])
-        
-        
-        optimizer = tf.keras.optimizers.Adam(lr=lr)
-        model.compile(loss='binary_crossentropy',
-                      optimizer=optimizer,
-                      metrics=['accuracy'])
+        # Model parameters
+        model = Sequential()
+        model.add(Flatten(input_shape=(X_train.shape[1],)))
 
-        # Train model
+        for lix in range(layers):
+            model.add(Dense(num_units, activation=activation))
+            if lix <= layers - 2:
+                model.add(Dropout(dropout))
+
+        model.add(Dense(1, activation="sigmoid"))
+
+        model.compile(
+            loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"],
+        )
+
         for i in range(epochs):
             model.fit(X_train, y_train)
             loss, accuracy = model.evaluate(X_test, y_test)
-            study.add_observation(trial=trial, iteration=i,
-                                  objective=accuracy,
-                                  context={'loss': loss})
+            study.add_observation(
+                trial=trial, iteration=i, objective=accuracy, context={"loss": loss}
+            )
             if study.should_trial_stop(trial):
-                break 
-        
-        study.finalize(trial=trial)    
+                print("Stopping Trial {} after {} iterations.".format(trial.id, i + 1))
+                study.finalize(trial=trial, status="STOPPED")
+                break
+                
+        print("Saving optimized hyperparamter settings")
+        study.finalize(trial=trial, status="COMPLETED")
+        np.save(sherpa_results_file, study.get_best_result())
 
 
-
-
-
-    
 if __name__ == "__main__":
     rinvs = ["0p0", "0p3", "1p0"]
-    layers = 5
-    nodes = 200
     for rinv in rinvs:
         # Grab jet images and labels
         X, y = get_data(rinv, N=20000)
 
         # Train a new model (or load the existing one if available)
         run_sherpa(X, y, rinv)
-    
+

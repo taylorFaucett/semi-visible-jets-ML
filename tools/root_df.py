4ceca87d769fb74a768da96586d1ac6979c26d3c
Taylor Faucett <tfaucett@uci.edu>
2020-12-11 06:28:31 -0700

    formatting sherpa/bootstrap code (no changes to function)

--- tools/root_df.py
+++ tools/root_df.py
@@ -24,16 +24,20 @@ def root_df():
     ]
 
     for rinv in rinvs:
-        rinv_path = os.path.join(home, "root_exports", rinv)
+        rinv_path = os.path.join(home, "data", "root_exports", rinv)
         if not os.path.exists(rinv_path):
             os.mkdir(rinv_path)
 
         root_files = glob.glob(
-            os.path.join(home, "data", "root_files", "rinv-" + rinv, "*.root")
+            os.path.join(
+                home, "generation", "data", "root_files", "rinv-" + rinv, "*.root"
+            )
         )
         for root_file in tqdm(root_files):
             run_name = os.path.basename(root_file).split(".root")[0]
-            output_file = os.path.join(home, "root_exports", rinv, run_name + ".h5")
+            output_file = os.path.join(
+                home, "data", "root_exports", rinv, run_name + ".h5"
+            )
             if not os.path.isfile(output_file):
                 for branches in branch_list:
                     pt_eta_phi_mass = export_data(root_file, branches)
